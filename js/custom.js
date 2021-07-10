41d825f1d0d283840cc6bd2e54943bdaffd214fe
mdmaruf641 <mdmarufmoni50@gmail.com>
2021-07-10 02:52:07 -0700

    banner slider added

--- js/custom.js
+++ js/custom.js
@@ -5,8 +5,16 @@ $(function(){
     $(".active").click(function(){
         $(".search-box").toggle();
         $("input[type='text']").focus();
-    })
+    });
 
+    $('.banner-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '.prev',
+        nextArrow: '.next',
+        dots: true,
+      });
 
 
 
